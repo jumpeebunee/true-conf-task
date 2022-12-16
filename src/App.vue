ac91311e9927bb8778a88b58776dab438c3e4294
Ben <jumpeebunee@gmail.com>
2022-12-16 10:00:33 +0300

    feat: add base styles and base config for elevator

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,32 @@
 <template>
-  <div>qq</div>
+  <main>
+    <div class="container">
+    </div>
+  </main>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      elevators: 1,
+      floors: 5,
+      stack: [],
+    }
+  }
 }
 </script>
 
 <style lang="scss">
+  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Lato:wght@300;400;700;900&display=swap');
+  
+  * {
+    font-family: Inter;
+    color: #fff;
+  }
+
+  body {
+    background-color: #1E1A20;
+  }
 
 </style>
